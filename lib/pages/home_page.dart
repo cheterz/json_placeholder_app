6de8cd57611631aab60da3ad9214852e48630f88
cheterz <cheterz32@gmail.com>
2021-06-17 10:21:38 +0300

    add counter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,16 +6,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
       body: Center(
+          child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 900),
+        reverseDuration: const Duration(milliseconds: 0),
         child: Text(
-          'Hello, JSON placeholder',
+          "$_counter",
+          key: ValueKey<int>(_counter),
           textAlign: TextAlign.center,
           style: Theme.of(context).textTheme.headline3,
         ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.animation),
+        onPressed: () {
+          setState(() {
+            _counter++;
+          });
+        },
       ),
     );
   }
